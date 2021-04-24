ff3e14159cbe372c4b3f36b2c3acec955587d5e7
Kris Litman <kris.d.litman@gmail.com>
2021-04-23 22:38:47 -0400

    Call for getting the lat and long is working, building service test and adding webmock and vcr

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::WeatherController < ApplicationController
   def get_forecast
+    coordinates = WeatherService.lat_and_long(params[:location])
 
+  #   conn = Faraday.new('http://www.mapquestapi.com')
+  #   response = conn.get("geocoding/v1/address") do |req|
+  #     req.params['location'] = params[:location]
+  #     req.params['key'] = Figaro.env.key
+  #   end
     
-    require 'pry'; binding.pry
+  # data = JSON.parse(response.body)
+
+  require 'pry'; binding.pry
   end
 end
