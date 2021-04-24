402404669806ccf977275b2406e01774bebcb7e9
Kris Litman <kris.d.litman@gmail.com>
2021-04-24 00:56:31 -0400

    Adding test for finding daily weather and the corresponding poro

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -12,16 +12,20 @@ RSpec.describe WeatherFacade do
       expect(expected.lat_and_long.lng).to eq(-104.984853)
     end
   end
-  it "Can find current weather" do
+  it "Can find current_weather" do
     VCR.use_cassette('current_weather') do
       facade = WeatherFacade.new("Denver,CO")
       weather = facade.current_weather
 
-      expect(expected).to be_an_instance_of(WeatherFacade)
-      expect(expected.location).to eq('Denver,CO')
-      expect(expected.lat_and_long).to be_an_instance_of(MapQuest)
-      expect(expected.lat_and_long.lat).to eq(39.738453)
-      expect(expected.lat_and_long.lng).to eq(-104.984853)
+      expect(weather).to be_an_instance_of(CurrentWeather)
+    end
+  end
+  it "Can find daily_weather" do
+    VCR.use_cassette('current_weather') do
+      facade = WeatherFacade.new("Denver,CO")
+      weather = facade.current_weather
+
+      expect(weather).to be_an_instance_of(CurrentWeather)
     end
   end
 end
