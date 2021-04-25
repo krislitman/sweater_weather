03e533f33bbac2b8f527fabce28f586a621d9bcb
Kris Litman <kris.d.litman@gmail.com>
2021-04-24 22:59:13 -0400

    Section 2 complete functionality wise, need to add testing

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,16 @@
 class Api::V1::UsersController < ApplicationController
   def register
-    
-    require 'pry'; binding.pry
+    user = User.new(user_params)
+    if user.save
+      render json: UsersSerializer.new(user), status: :created
+    else
+      invalid_registration(user.errors.full_messages)
+    end
   end
 
   private
 
   def user_params
-    params.permit(:email, :password)
+    params.permit(:email, :password, :password_confirmation)
   end
 end
