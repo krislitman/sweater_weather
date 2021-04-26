afa47a54afdfc39efc62b3a02ee3c14d32642da3
Kris Litman <kris.d.litman@gmail.com>
2021-04-26 11:12:14 -0400

    Added allowing http requests when no cassette is in use to rails helper. Building test for Teleport API call in requests.

---
+++ spec/requests/job_salaries_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'  
+
+RSpec.describe 'Destination City Forecast And Salary', type: :request do
+  scenario 'Happy Path ~ Teleport API finds tech salary for destination city' do
+    params = {
+      destination: 'chicago'
+    }
+    get api_v1_destination_path, params: params
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+  end
+end
