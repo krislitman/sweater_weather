fccdf0b115f847761deb82e3774ac70fd2186627
Kris Litman <kris.d.litman@gmail.com>
2021-04-26 12:24:22 -0400

    Refactored all code from controller into facade. Going to build out salariesservice next

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,27 +1,6 @@
 class Api::V1::SalariesController < ApplicationController
   def tech_salaries
-    response = conn.get("api/urban_areas/slug:#{params[:destination]}/salaries")
-    expected = JSON.parse(response.body, symbolize_names: true)
-    jobs = expected[:salaries].map do |data|
-      Job.new(data)
-    end
-    relevant_jobs(jobs)
-  end
-
-  def relevant_jobs(jobs)
-    expected = jobs.find_all do |job|
-      job.title == 'Data Analyst' || job.title == 'Data Scientist' ||
-      job.title == 'Mobile Developer' || job.title == 'QA Engineer' ||
-      job.title == 'Software Engineer' || job.title == 'Systems Administrator' ||
-      job.title == 'Web Developer'
-    end
-    salaries = SalariesFacade.new(expected, params[:destination])
+    salaries = SalariesFacade.new(params[:destination])
     render json: SalariesSerializer.new(salaries)
-  end
-
-  private
-
-  def conn
-    Faraday.new('http://api.teleport.org')
   end
 end
--- app/facades/salaries_facade.rb
+++ app/facades/salaries_facade.rb
@@ -3,10 +3,19 @@ class SalariesFacade
               :forecast,
               :salaries
 
-  def initialize(salaries, destination)
+  def initialize(destination)
     @destination = destination
     @forecast = find_forecast
-    @salaries = salaries
+    @salaries = find_salaries
+  end
+
+  def find_salaries
+    response = conn.get("api/urban_areas/slug:#{@destination}/salaries")
+    expected = JSON.parse(response.body, symbolize_names: true)
+    jobs = expected[:salaries].map do |data|
+      Job.new(data)
+    end
+    relevant_jobs(jobs)
   end
 
   def find_forecast
@@ -15,5 +24,20 @@ class SalariesFacade
       summary: facade.current_weather.conditions.titleize,
       temperature: facade.current_weather.temperature
     }
+  end
+
+  def relevant_jobs(jobs)
+    jobs.find_all do |job|
+      job.title == 'Data Analyst' || job.title == 'Data Scientist' ||
+      job.title == 'Mobile Developer' || job.title == 'QA Engineer' ||
+      job.title == 'Software Engineer' || job.title == 'Systems Administrator' ||
+      job.title == 'Web Developer'
+    end
+  end
+
+  private
+
+  def conn
+    Faraday.new('http://api.teleport.org')
   end
 end
